3722fff8c85f5f96e54e0a32f5de87784363dcd6
Alexander Pristavko <pristavkoal@gmail.com>
2018-10-23 12:51:15 +0300

    Adjust tests

--- MapboxVisionTests/RecordSynchronizerTests.swift
+++ MapboxVisionTests/RecordSynchronizerTests.swift
@@ -55,6 +55,9 @@ class RecordSynchronizerTests: XCTestCase, SyncDelegate {
                 File(url: URL(fileURLWithPath: "/1/1.mp4"), size: 1),
                 File(url: URL(fileURLWithPath: "/1/2.mp4"), size: 1),
                 File(url: URL(fileURLWithPath: "/1/3.mp4"), size: 1),
+                File(url: URL(fileURLWithPath: "/1/images/1.jpg"), size: 1),
+                File(url: URL(fileURLWithPath: "/1/images/2.jpg"), size: 1),
+                File(url: URL(fileURLWithPath: "/1/images/3.jpg"), size: 1),
             ],
             URL(fileURLWithPath: "/2", isDirectory: true) : [
                 File(url: URL(fileURLWithPath: "/2/gps.bin"), size: 15),
@@ -62,13 +65,16 @@ class RecordSynchronizerTests: XCTestCase, SyncDelegate {
                 File(url: URL(fileURLWithPath: "/2/1.mp4"), size: 1),
                 File(url: URL(fileURLWithPath: "/2/2.mp4"), size: 1),
                 File(url: URL(fileURLWithPath: "/2/3.mp4"), size: 1),
+                File(url: URL(fileURLWithPath: "/2/images/1.jpg"), size: 1),
+                File(url: URL(fileURLWithPath: "/2/images/2.jpg"), size: 1),
+                File(url: URL(fileURLWithPath: "/2/images/3.jpg"), size: 1),
             ],
             URL(fileURLWithPath: "/3", isDirectory: true) : [
                 File(url: URL(fileURLWithPath: "/3/.synced"), size: 0)
             ]
         ]
         
-        fileManager.data = data
+        fileManager.data = data.values.flatMap { $0 }
         dataSource.recordDirectories = data.keys.map { $0 }
         
         recordSynchronizer.sync()
@@ -81,46 +87,44 @@ class RecordSynchronizerTests: XCTestCase, SyncDelegate {
     
     func syncStopped() {
         
-        XCTAssert(archiver.archives.count == 2, "Archiver should create 2 archives");
+        let archives = [
+            URL(fileURLWithPath: "/1/telemetry.zip"),
+            URL(fileURLWithPath: "/2/telemetry.zip"),
+            URL(fileURLWithPath: "/1/images.zip"),
+            URL(fileURLWithPath: "/2/images.zip"),
+        ]
         
-        let firstArchive = URL(fileURLWithPath: "/1/telemetry.zip")
-        let secondArchive = URL(fileURLWithPath: "/2/telemetry.zip")
+        XCTAssert(archiver.archives.count == archives.count, "Archiver should create \(archives.count) archives");
         
-        XCTAssertNotNil(archiver.archives[firstArchive], "Archiver should create archive with right path");
-        XCTAssertNotNil(archiver.archives[secondArchive], "Archiver should create archive with right path");
+        archives.forEach { (archive) in
+            XCTAssertNotNil(archiver.archives[archive], "Archiver should create archive with right path")
+            let uploadDir = networkClient.uploaded[archive]
+            let dir = "\(archive.pathComponents[1])_en_US_DEVICE_ID_iOS"
+            XCTAssert(uploadDir == dir, "\(archive) should be uploaded to \(dir). Actual upload dir: \(uploadDir ?? "none")")
+            XCTAssert(dataSource.removedFiles.contains(archive), "\(archive) should be removed after upload")
+        }
         
         XCTAssert(dataSource.removedFiles.contains(URL(fileURLWithPath: "/1/gps.bin")), "Bin file should be removed after archivation")
         XCTAssert(dataSource.removedFiles.contains(URL(fileURLWithPath: "/1/videos.json")), "Json file should be removed after archivation")
         
-        XCTAssert(networkClient.uploaded[firstArchive] == "1_en_US_DEVICE_ID_iOS", "First archive should be uploaded to right dir")
-        XCTAssert(networkClient.uploaded[secondArchive] == "2_en_US_DEVICE_ID_iOS", "Second archive should be uploaded to right dir")
-        
-        XCTAssert(dataSource.removedFiles.contains(firstArchive), "First archive should be removed after upload")
-        XCTAssert(dataSource.removedFiles.contains(secondArchive), "Second archive should be removed after upload")
-        
         XCTAssert(fileManager.fileExists(atPath: "/1/.synced"), "Telemetry from first dir should be marked as synced")
         XCTAssert(fileManager.fileExists(atPath: "/2/.synced"), "Telemetry from second dir should be marked as synced")
         
-        let video1_1 = URL(fileURLWithPath: "/1/1.mp4")
-        let video1_2 = URL(fileURLWithPath: "/1/2.mp4")
-        let video1_3 = URL(fileURLWithPath: "/1/3.mp4")
-        let video2_1 = URL(fileURLWithPath: "/2/1.mp4")
-        let video2_2 = URL(fileURLWithPath: "/2/2.mp4")
-        let video2_3 = URL(fileURLWithPath: "/2/3.mp4")
+        let files = [
+            URL(fileURLWithPath: "/1/1.mp4"),
+            URL(fileURLWithPath: "/1/2.mp4"),
+            URL(fileURLWithPath: "/1/3.mp4"),
+            URL(fileURLWithPath: "/2/1.mp4"),
+            URL(fileURLWithPath: "/2/2.mp4"),
+            URL(fileURLWithPath: "/2/3.mp4"),
+        ]
         
-        XCTAssert(networkClient.uploaded[video1_1] == "1_en_US_DEVICE_ID_iOS", "Video from first dir should be uploaded to right dir")
-        XCTAssert(networkClient.uploaded[video1_2] == "1_en_US_DEVICE_ID_iOS", "Video from first dir should be uploaded to right dir")
-        XCTAssert(networkClient.uploaded[video1_3] == "1_en_US_DEVICE_ID_iOS", "Video from first dir should be uploaded to right dir")
-        XCTAssert(networkClient.uploaded[video2_1] == "2_en_US_DEVICE_ID_iOS", "Video from second dir should be uploaded to right dir")
-        XCTAssert(networkClient.uploaded[video2_2] == "2_en_US_DEVICE_ID_iOS", "Video from second dir should be uploaded to right dir")
-        XCTAssert(networkClient.uploaded[video2_3] == "2_en_US_DEVICE_ID_iOS", "Video from second dir should be uploaded to right dir")
-        
-        XCTAssert(dataSource.removedFiles.contains(video1_1), "Video should be removed after upload")
-        XCTAssert(dataSource.removedFiles.contains(video1_2), "Video should be removed after upload")
-        XCTAssert(dataSource.removedFiles.contains(video1_3), "Video should be removed after upload")
-        XCTAssert(dataSource.removedFiles.contains(video2_1), "Video should be removed after upload")
-        XCTAssert(dataSource.removedFiles.contains(video2_2), "Video should be removed after upload")
-        XCTAssert(dataSource.removedFiles.contains(video2_3), "Video should be removed after upload")
+        files.forEach { (file) in
+            let uploadDir = networkClient.uploaded[file]
+            let dir = "\(file.pathComponents[1])_en_US_DEVICE_ID_iOS"
+            XCTAssert(uploadDir == dir, "\(file) should be uploaded to \(dir). Actual upload dir: \(uploadDir ?? "none")")
+            XCTAssert(dataSource.removedFiles.contains(file), "\(file) should be removed after upload")
+        }
         
         positiveScenarioExpectation.fulfill()
     }
