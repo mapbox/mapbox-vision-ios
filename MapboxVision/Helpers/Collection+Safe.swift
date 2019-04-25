47b34cd6125da7dbff9312459136e64494af233d
Dersim Davaod <dersim.davaod@mapbox.com>
2019-04-25 16:06:59 +0300

    Update code according to `Space around parens` rule from swiftformat.

--- MapboxVision/Helpers/Collection+Safe.swift
+++ MapboxVision/Helpers/Collection+Safe.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 extension Collection {
-    subscript (safe index: Index) -> Element? {
+    subscript(safe index: Index) -> Element? {
         return indices.contains(index) ? self[index] : nil
     }
 }
