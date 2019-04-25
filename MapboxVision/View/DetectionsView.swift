90bb7d8c734e7379056cb7463b4e47226d4be856
Dersim Davaod <dersim.davaod@mapbox.com>
2019-04-25 12:30:47 +0300

    Fix `Operator Usage Whitespace Violation` warnings from swiftlint.

--- MapboxVision/View/DetectionsView.swift
+++ MapboxVision/View/DetectionsView.swift
@@ -53,15 +53,15 @@ private extension DetectionClass {
     var color: UIColor {
         switch self {
         case .trafficLight:
-            return UIColor(red: 6.0/255.0, green: 241.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+            return UIColor(red: 6.0 / 255.0, green: 241.0 / 255.0, blue: 255.0 / 255.0, alpha: 1.0)
         case .trafficSign:
-            return UIColor(red: 255.0/255.0, green: 204.0/255.0, blue: 22.0/255.0, alpha: 1.0)
+            return UIColor(red: 255.0 / 255.0, green: 204.0 / 255.0, blue: 22.0 / 255.0, alpha: 1.0)
         case .car:
-            return UIColor(red: 144.0/255.0, green: 255.0/255.0, blue: 22.0/255.0, alpha: 1.0)
+            return UIColor(red: 144.0 / 255.0, green: 255.0 / 255.0, blue: 22.0 / 255.0, alpha: 1.0)
         case .person:
-            return UIColor(red: 239.0/255.0, green: 6.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+            return UIColor(red: 239.0 / 255.0, green: 6.0 / 255.0, blue: 255.0 / 255.0, alpha: 1.0)
         case .bicycle:
-            return UIColor(red: 0, green: 165.0/255.0, blue: 1.0, alpha: 1.0)
+            return UIColor(red: 0, green: 165.0 / 255.0, blue: 1.0, alpha: 1.0)
         }
     }
 
