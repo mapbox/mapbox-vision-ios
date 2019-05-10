ea61d2e4d2b280de4595190c311c2670190c51a3
Dersim Davaod <dersim.davaod@mapbox.com>
2019-05-10 14:38:09 +0300

    Add stubs for UIDevice class to override modelIDs.

--- MapboxVisionTests/Mocks/UIDeviceStubs.swift
+++ MapboxVisionTests/Mocks/UIDeviceStubs.swift
@@ -4,3 +4,142 @@
 //
 
 import Foundation
+import MapboxVision
+
+final class UIDeviceIphone5sGSMStub: UIDevice {
+    override var modelID: String {
+        return "iPhone6,1"
+    }
+}
+
+final class UIDeviceIphone5sGlobalStub: UIDevice {
+    override var modelID: String {
+        return "iPhone6,2"
+    }
+}
+
+final class UIDeviceIphone6PlusStub: UIDevice {
+    override var modelID: String {
+        return "iPhone7,1"
+    }
+}
+
+final class UIDeviceIphone6Stub: UIDevice {
+    override var modelID: String {
+        return "iPhone7,2"
+    }
+}
+
+final class UIDeviceIphone6sStub: UIDevice {
+    override var modelID: String {
+        return "iPhone8,1"
+    }
+}
+
+final class UIDeviceIphone6sPlusStub: UIDevice {
+    override var modelID: String {
+        return "iPhone8,2"
+    }
+}
+
+final class UIDeviceIphoneSEStub: UIDevice {
+    override var modelID: String {
+        return "iPhone8,4"
+    }
+}
+
+final class UIDeviceIphone7CDMAStub: UIDevice {
+    override var modelID: String {
+        return "iPhone9,1"
+    }
+}
+
+final class UIDeviceIphone7GSMStub: UIDevice {
+    override var modelID: String {
+        return "iPhone9,3"
+    }
+}
+
+final class UIDeviceIphone7PlusCDMAStub: UIDevice {
+    override var modelID: String {
+        return "iPhone9,2"
+    }
+}
+
+final class UIDeviceIphone7PlusGSMStub: UIDevice {
+    override var modelID: String {
+        return "iPhone9,4"
+    }
+}
+
+final class UIDeviceIphone8CDMAStub: UIDevice {
+    override var modelID: String {
+        return "iPhone10,1"
+    }
+}
+
+final class UIDeviceIphone8GSMStub: UIDevice {
+    override var modelID: String {
+        return "iPhone10,4"
+    }
+}
+
+final class UIDeviceIphone8PlusCDMAStub: UIDevice {
+    override var modelID: String {
+        return "iPhone10,2"
+    }
+}
+
+final class UIDeviceIphone8PlusGSMStub: UIDevice {
+    override var modelID: String {
+        return "iPhone10,5"
+    }
+}
+
+final class UIDeviceIphoneXCDMAStub: UIDevice {
+    override var modelID: String {
+        return "iPhone10,3"
+    }
+}
+
+final class UIDeviceIphoneXGSMStub: UIDevice {
+    override var modelID: String {
+        return "iPhone10,6"
+    }
+}
+
+final class UIDeviceIphoneXSStub: UIDevice {
+    override var modelID: String {
+        return "iPhone11,2"
+    }
+}
+
+final class UIDeviceIphoneXSMaxStub: UIDevice {
+    override var modelID: String {
+        return "iPhone11,4"
+    }
+}
+
+final class UIDeviceIphoneXSMaxChinaStub: UIDevice {
+    override var modelID: String {
+        return "iPhone11,6"
+    }
+}
+
+final class UIDeviceIphoneXRStub: UIDevice {
+    override var modelID: String {
+        return "iPhone11,8"
+    }
+}
+
+final class UIDeviceIphoneNextGenerationStub: UIDevice {
+    override var modelID: String {
+        return "iPhone12,1"
+    }
+}
+
+final class UIDeviceIpadStub: UIDevice {
+    override var modelID: String {
+        return "iPad10,2"
+    }
+}
