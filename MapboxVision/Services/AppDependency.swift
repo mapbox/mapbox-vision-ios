352194c344ed56ed4feb1fa4b2c493f427bedb0b
Alexander Pristavko <pristavkoal@gmail.com>
2019-05-17 09:56:59 +0300

    Extract common functionality to base vision manager

--- MapboxVision/Services/AppDependency.swift
+++ MapboxVision/Services/AppDependency.swift
@@ -12,33 +12,26 @@ import MapboxVisionNative
 private let visionSessionInterval: TimeInterval = 5 * 60
 private let visionVideoSettings: VideoSettings = .lowQuality
 
-protocol VisionDependency {
-    var synchronizer: Synchronizable { get }
-    var recorder: SessionRecorder { get }
-    var native: VisionManagerNative { get }
-    var locationManager: LocationManager { get }
-    var motionManager: MotionManager { get }
-    var deviceInfo: DeviceInfoProvidable { get }
+struct BaseDependencies {
+    let native: VisionManagerBaseNative
+    let synchronizer: Synchronizable
 }
 
-final class AppDependency: VisionDependency {
-    private(set) var synchronizer: Synchronizable
-    private(set) var native: VisionManagerNative
-    private(set) var recorder: SessionRecorder
-    private(set) var locationManager: LocationManager
-    private(set) var motionManager: MotionManager
-    private(set) var deviceInfo: DeviceInfoProvidable
-    private let eventsManager = EventsManager()
-    private let platform: Platform
-    
-    init(operationMode: OperationMode) {
-        
+struct VisionDependencies {
+    let native: VisionManagerNative
+    let synchronizer: Synchronizable
+    let recorder: SessionRecorder
+    let dataProvider: DataProvider
+    let deviceInfo: DeviceInfoProvidable
+
+    static func `default`() -> VisionDependencies {
         guard let reachability = Reachability() else {
             fatalError("Reachability failed to initialize")
         }
-        
-        self.deviceInfo = DeviceInfoProvider()
-        
+
+        let eventsManager = EventsManager()
+        let deviceInfo = DeviceInfoProvider()
+
         let dataSource = SyncRecordDataSource()
         let recordArchiver = RecordArchiver()
         let recordSyncDependencies = RecordSynchronizer.Dependencies(
@@ -54,19 +47,19 @@ final class AppDependency: VisionDependency {
             base: recordSynchronizer,
             reachability: reachability
         )
-        self.synchronizer = ManagedSynchronizer(dependencies: syncDependencies)
-        
-        let recorder = RecordCoordinator()
-        
-        self.platform = Platform(dependencies: Platform.Dependencies(
-            recordCoordinator: recorder,
+        let synchronizer = ManagedSynchronizer(dependencies: syncDependencies)
+
+        let recordCoordinator = RecordCoordinator()
+
+        let platform = Platform(dependencies: Platform.Dependencies(
+            recordCoordinator: recordCoordinator,
             eventsManager: eventsManager
         ))
-        
-        self.native = VisionManagerNative.create(withPlatform: platform)
-        
-        self.recorder = SessionRecorder(dependencies: SessionRecorder.Dependencies(
-            recorder: recorder,
+
+        let native = VisionManagerNative.create(withPlatform: platform)
+
+        let recorder = SessionRecorder(dependencies: SessionRecorder.Dependencies(
+            recorder: recordCoordinator,
             sessionManager: SessionManager(),
             videoSettings: visionVideoSettings,
             sessionInterval: visionSessionInterval,
@@ -74,8 +67,17 @@ final class AppDependency: VisionDependency {
             startSavingSession: native.startSavingSession,
             stopSavingSession: native.stopSavingSession
         ))
-        
-        self.locationManager = LocationManager()
-        self.motionManager = MotionManager(with: platform.getMotionReferenceFrame())
+
+        let dataProvider = RealtimeDataProvider(dependencies: RealtimeDataProvider.Dependencies(
+            native: native,
+            motionManager: MotionManager(with: platform.getMotionReferenceFrame()),
+            locationManager: LocationManager()
+        ))
+
+        return VisionDependencies(native: native,
+                                  synchronizer: synchronizer,
+                                  recorder: recorder,
+                                  dataProvider: dataProvider,
+                                  deviceInfo: deviceInfo)
     }
 }
--- MapboxVision/VisionManager/VisionManagerDelegate.swift
+++ MapboxVision/VisionManager/VisionManagerDelegate.swift
@@ -21,54 +21,54 @@ public protocol VisionManagerDelegate: class {
         Tells the delegate that authorization status was updated.
         `VisionManager` may not emit events unless it has successfully authorized.
     */
-    func visionManager(_ visionManager: VisionManager, didUpdateAuthorizationStatus authorizationStatus: AuthorizationStatus) -> Void
+    func visionManager(_ visionManager: BaseVisionManager, didUpdateAuthorizationStatus authorizationStatus: AuthorizationStatus)
     
     /**
         Tells the delegate that segmentation mask was updated.
         Requires at least low performance for segmentation.
     */
-    func visionManager(_ visionManager: VisionManager, didUpdateFrameSegmentation frameSegmentation: FrameSegmentation) -> Void
+    func visionManager(_ visionManager: BaseVisionManager, didUpdateFrameSegmentation frameSegmentation: FrameSegmentation)
     
     /**
         Tells the delegate that detections were updated.
         Requires at least low performance for detection.
     */
-    func visionManager(_ visionManager: VisionManager, didUpdateFrameDetections frameDetections: FrameDetections) -> Void
+    func visionManager(_ visionManager: BaseVisionManager, didUpdateFrameDetections frameDetections: FrameDetections)
     
     /**
         Tells the delegate that classified signs were updated.
         Requires at least low performance for detection.
     */
-    func visionManager(_ visionManager: VisionManager, didUpdateFrameSignClassifications frameSignClassifications: FrameSignClassifications) -> Void
+    func visionManager(_ visionManager: BaseVisionManager, didUpdateFrameSignClassifications frameSignClassifications: FrameSignClassifications)
     
     /**
         Tells the delegate that road description was updated.
         Road description parameters reach maximum accuracy when `Camera` is calibrated.
         Requires at least low performance for segmentation.
     */
-    func visionManager(_ visionManager: VisionManager, didUpdateRoadDescription roadDescription: RoadDescription) -> Void
+    func visionManager(_ visionManager: BaseVisionManager, didUpdateRoadDescription roadDescription: RoadDescription)
     
     /**
         Tells the delegate that world description was updated.
         World description parameters reach maximum accuracy when `Camera` is calibrated.
         Requires at least low performance for detection.
     */
-    func visionManager(_ visionManager: VisionManager, didUpdateWorldDescription worldDescription: WorldDescription) -> Void
+    func visionManager(_ visionManager: BaseVisionManager, didUpdateWorldDescription worldDescription: WorldDescription)
     
     /**
         Tells the delegate that vehicle state was updated.
     */
-    func visionManager(_ visionManager: VisionManager, didUpdateVehicleState vehicleState: VehicleState) -> Void
+    func visionManager(_ visionManager: BaseVisionManager, didUpdateVehicleState vehicleState: VehicleState)
     
     /**
         Tells the delegate that country which is used in the VisionSDK changed.
     */
-    func visionManager(_ visionManager: VisionManager, didUpdateCountry country: Country) -> Void
+    func visionManager(_ visionManager: BaseVisionManager, didUpdateCountry country: Country)
     
     /**
         Tells the delegate that camera state was updated.
     */
-    func visionManager(_ visionManager: VisionManager, didUpdateCamera camera: Camera) -> Void
+    func visionManager(_ visionManager: BaseVisionManager, didUpdateCamera camera: Camera)
     
     /**
         This method is called after the whole update iteration is completed. This means that all the data which came from delegate methods is in sync.
@@ -76,27 +76,27 @@ public protocol VisionManagerDelegate: class {
         
         - NOTE: Performance of this function is critical since `VisionManager` blocks until the method execution is finished.
     */
-    func visionManagerDidCompleteUpdate(_ visionManager: VisionManager) -> Void
+    func visionManagerDidCompleteUpdate(_ visionManager: BaseVisionManager)
 }
 
 public extension VisionManagerDelegate {
-    func visionManager(_ visionManager: VisionManager, didUpdateAuthorizationStatus authorizationStatus: AuthorizationStatus) -> Void {}
+    func visionManager(_ visionManager: BaseVisionManager, didUpdateAuthorizationStatus authorizationStatus: AuthorizationStatus) {}
     
-    func visionManager(_ visionManager: VisionManager, didUpdateFrameSegmentation frameSegmentation: FrameSegmentation) -> Void {}
+    func visionManager(_ visionManager: BaseVisionManager, didUpdateFrameSegmentation frameSegmentation: FrameSegmentation) {}
     
-    func visionManager(_ visionManager: VisionManager, didUpdateFrameDetections frameDetections: FrameDetections) -> Void {}
+    func visionManager(_ visionManager: BaseVisionManager, didUpdateFrameDetections frameDetections: FrameDetections) {}
     
-    func visionManager(_ visionManager: VisionManager, didUpdateFrameSignClassifications frameSignClassifications: FrameSignClassifications) -> Void {}
+    func visionManager(_ visionManager: BaseVisionManager, didUpdateFrameSignClassifications frameSignClassifications: FrameSignClassifications) {}
     
-    func visionManager(_ visionManager: VisionManager, didUpdateRoadDescription roadDescription: RoadDescription) -> Void {}
+    func visionManager(_ visionManager: BaseVisionManager, didUpdateRoadDescription roadDescription: RoadDescription) {}
     
-    func visionManager(_ visionManager: VisionManager, didUpdateWorldDescription worldDescription: WorldDescription) -> Void {}
+    func visionManager(_ visionManager: BaseVisionManager, didUpdateWorldDescription worldDescription: WorldDescription) {}
     
-    func visionManager(_ visionManager: VisionManager, didUpdateVehicleState vehicleState: VehicleState) -> Void {}
+    func visionManager(_ visionManager: BaseVisionManager, didUpdateVehicleState vehicleState: VehicleState) {}
     
-    func visionManager(_ visionManager: VisionManager, didUpdateCamera camera: Camera) -> Void {}
+    func visionManager(_ visionManager: BaseVisionManager, didUpdateCamera camera: Camera) {}
     
-    func visionManager(_ visionManager: VisionManager, didUpdateCountry country: Country) -> Void {}
+    func visionManager(_ visionManager: BaseVisionManager, didUpdateCountry country: Country) {}
     
-    func visionManagerDidCompleteUpdate(_ visionManager: VisionManager) -> Void {}
+    func visionManagerDidCompleteUpdate(_ visionManager: BaseVisionManager) {}
 }
