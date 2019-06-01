93dccb62b754b3b9c59d0f8bdaacc058eb8f9bbf
Dersim Davaod <dersim.davaod@mapbox.com>
2019-06-01 17:32:16 +0300

    Remove ARGridNode and unit tests.

--- MapboxVisionAR/Render/ARScene/ARNode/ARNodeType.swift
+++ MapboxVisionAR/Render/ARScene/ARNode/ARNodeType.swift
@@ -2,8 +2,6 @@
 enum ARNodeType {
     /// The type represents `ARRootNode`.
     case rootNode
-    /// The type represents `ARGridNode`.
-    case gridNode
     /// The type represents `ARLaneNode`.
     case arrowNode
     /// The type represents `ARCameraNode`.
