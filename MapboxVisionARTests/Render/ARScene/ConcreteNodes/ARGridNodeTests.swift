064a1948379191c048207f1c901cab7a54f08af6
Dersim Davaod <dersim.davaod@mapbox.com>
2019-06-01 09:51:41 +0300

    Add basic ARGridNodeTests.

--- MapboxVisionARTests/Render/ARScene/ConcreteNodes/ARGridNodeTests.swift
+++ MapboxVisionARTests/Render/ARScene/ConcreteNodes/ARGridNodeTests.swift
@@ -1,3 +1,22 @@
-//
+@testable import MapboxVisionAR
+import XCTest
 
-import Foundation
+class ARGridNodeTests: XCTestCase {
+    private var rootNode: ARGridNode!
+
+    override func setUp() {
+        super.setUp()
+        rootNode = ARGridNode()
+    }
+    func testARGridNodeHasGridNodeType() {
+        // Given state from setUp()
+        // When // Then
+        XCTAssertEqual(rootNode.nodeType, .gridNode)
+    }
+
+    func testARGridNodeDoesNotHaveAREntity() {
+        // Given state from setUp()
+        // When // Then
+        XCTAssertNil(rootNode.entity)
+    }
+}
