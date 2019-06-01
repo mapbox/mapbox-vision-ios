a4099236732300ea1e6970c868613c1b29a2fbec
Dersim Davaod <dersim.davaod@mapbox.com>
2019-06-01 09:47:50 +0300

    Add basic doc for Node protocol.

--- MapboxVisionAR/Render/ARScene/Node/NodeProtocol/Node.swift
+++ MapboxVisionAR/Render/ARScene/Node/NodeProtocol/Node.swift
@@ -1,7 +1,24 @@
+/// Methods and properties common to the nodes which will be displayed in `ARScene`.
 protocol Node: AnyObject {
+    // MARK: - Properties
+
+    /// Set of properties describing node in the node hierarchy.
     var relations: NodeRelations { get set }
+    /// Set of properties that determine the transformation between coordinate systems.
     var geometry: NodeGeometry { get set }
 
+    // MARK: - Functions
+
+    /**
+     Adds a node to the node’s array of children.
+
+     - Parameters:
+       - child: The node to be added.
+     */
     func add(child: Node)
+
+    /**
+     Removes all child nodes from the node’s array of children.
+     */
     func removeAllChilds()
 }
