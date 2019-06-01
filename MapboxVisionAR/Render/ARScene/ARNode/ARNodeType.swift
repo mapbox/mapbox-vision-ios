599d5bd489748b734c0b90001f0b5d3eef881f3b
Dersim Davaod <dersim.davaod@mapbox.com>
2019-06-01 09:48:05 +0300

    Add basic doc for ARNodeType enum.

--- MapboxVisionAR/Render/ARScene/ARNode/ARNodeType.swift
+++ MapboxVisionAR/Render/ARScene/ARNode/ARNodeType.swift
@@ -1,6 +1,11 @@
+/// Specifies the concrete type of `ARNode`.
 enum ARNodeType {
+    /// The type represents `ARRootNode`.
     case rootNode
+    /// The type represents `ARGridNode`.
     case gridNode
+    /// The type represents `ARLaneNode`.
     case arrowNode
+    /// The type represents `ARCameraNode`.
     case cameraNode
 }
