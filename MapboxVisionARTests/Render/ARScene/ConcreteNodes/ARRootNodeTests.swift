ce026fce99cddf3e560a98106d779b8f127aa32a
Dersim Davaod <dersim.davaod@mapbox.com>
2019-06-01 09:51:30 +0300

    Add basic ARRootNodeTests.

--- MapboxVisionARTests/Render/ARScene/ConcreteNodes/ARRootNodeTests.swift
+++ MapboxVisionARTests/Render/ARScene/ConcreteNodes/ARRootNodeTests.swift
@@ -1,3 +1,22 @@
-//
+@testable import MapboxVisionAR
+import XCTest
 
-import Foundation
+class ARRootNodeTests: XCTestCase {
+    private var rootNode: ARRootNode!
+
+    override func setUp() {
+        super.setUp()
+        rootNode = ARRootNode()
+    }
+    func testARRootNodeHasRootNodeType() {
+        // Given state from setUp()
+        // When // Then
+        XCTAssertEqual(rootNode.nodeType, .rootNode)
+    }
+
+    func testARRootNodeDoesNotHaveAREntity() {
+        // Given state from setUp()
+        // When // Then
+        XCTAssertNil(rootNode.entity)
+    }
+}
