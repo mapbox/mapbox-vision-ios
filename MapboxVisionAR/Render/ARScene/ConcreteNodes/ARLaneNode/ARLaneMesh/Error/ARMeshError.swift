4e807787610a75aa6ce882b76f81cdd9add5f357
Dersim Davaod <dersim.davaod@mapbox.com>
2019-06-05 18:46:41 +0300

    Update ARMeshError: Provide partial conformance to `LocalizedError` protocol.

--- MapboxVisionAR/Render/ARScene/ConcreteNodes/ARLaneNode/ARLaneMesh/Error/ARMeshError.swift
+++ MapboxVisionAR/Render/ARScene/ConcreteNodes/ARLaneNode/ARLaneMesh/Error/ARMeshError.swift
@@ -4,4 +4,13 @@ enum ARMeshError: LocalizedError {
     case cantFindMeshFile(String)
     /// Mesh file is empty. Parametrized with name of resource file that contains mesh.
     case meshFileIsEmpty(String)
+
+    var errorDescription: String? {
+        switch self {
+        case .cantFindMeshFile(let fileName):
+            return "Mesh file \(fileName) can't be found."
+        case .meshFileIsEmpty(let fileName):
+            return "Mesh file \(fileName) is empty."
+        }
+    }
 }
