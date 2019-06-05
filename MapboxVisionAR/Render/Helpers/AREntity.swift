62800a687ba3bfaf019ee3a07f0d8841dc39e179
Dersim Davaod <dersim.davaod@mapbox.com>
2019-06-05 19:41:09 +0300

    Minor updates in comments for `AREntity`.

--- MapboxVisionAR/Render/Helpers/AREntity.swift
+++ MapboxVisionAR/Render/Helpers/AREntity.swift
@@ -1,17 +1,17 @@
 import MetalKit
 
 /**
- `AREntity` allows to more easily manage complex object graphs in your game.
+ `AREntity` allows to more easily manage complex object graphs.
  Basically it contains node-specific details.
  */
 class AREntity {
     // MARK: - Properties
 
-    ///
+    /// A container for the vertex data suitable for use in a Metal app.
     var mesh: MTKMesh?
     /// Defines the appearance of a geometry's surface when rendered.
     var material = ARMaterial()
-    ///
+    /// An object that contains the graphics functions and configuration state used in a render pass.
     var renderPipeline: MTLRenderPipelineState?
 
     // MARK: - Lifecycle
