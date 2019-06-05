03bc5e456d0a099de12d3790894a9779fd59c320
Dersim Davaod <dersim.davaod@mapbox.com>
2019-06-05 18:43:17 +0300

    Update ARRendererError: Provide partial conformance to `LocalizedError` protocol.

--- MapboxVisionAR/Render/Renderer/Error/ARRendererError.swift
+++ MapboxVisionAR/Render/Renderer/Error/ARRendererError.swift
@@ -8,4 +8,17 @@ enum ARRendererError: LocalizedError {
     case cantCreateBuffer
     /// One of required shared function can't be found.
     case cantFindFunctions
+
+    var errorDescription: String? {
+        switch self {
+        case .cantCreateCommandQueue:
+            return "A queue that organizes command buffers to be executed by a GPU can't be created."
+        case .cantCreateTextureCache:
+            return "Texture cache can't be created."
+        case .cantCreateBuffer:
+            return "Buffer for Metal device can't be created."
+        case .cantFindFunctions:
+            return "One of required shared function can't be found."
+        }
+    }
 }
