29cb13f190fe798fdfc263d37b769b2e7acf9b01
Dersim Davaod <dersim.davaod@mapbox.com>
2019-06-25 11:15:50 +0300

    Add git hooks manager via Swift Package Manager.

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:4.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "[vision-ios-examples]",
+    dependencies: [
+      .package(url: "https://github.com/orta/Komondor.git", from: "1.0.4")
+    ]
+)
+
+#if canImport(PackageConfig)
+    import PackageConfig
+
+    let config = PackageConfiguration([
+        "komondor": [
+            "pre-commit": "echo 'Hi'"
+        ],
+    ]).write()
+#endif
