79974d61d91feb64b487b7948eca13d031bea394
Dersim Davaod <dersim.davaod@mapbox.com>
2019-06-28 18:56:59 +0300

    Update `LaneVisualParams.swift`: set initial values to properties in order to get rid of default initializer.

--- MapboxVisionAR/ViewController/LaneVisualParams.swift
+++ MapboxVisionAR/ViewController/LaneVisualParams.swift
@@ -3,27 +3,19 @@
  */
 public struct LaneVisualParams {
     /// Color of the lane represented in RGBA. It takes alpha channel into account.
-    var color: UIColor
-    /// Width of the lane.
-    var width: Float
-    /// Position of the light source.
-    var lightPosition: WorldCoordinate
-    /// Color of the light source. It does not take alpha channel into account.
-    var lightColor: UIColor
-    /// Ambient light color represented in RGB. It does not take alpha channel into account.
-    var ambientColor: UIColor
-
-    init() {
-        color = UIColor(red: CGFloat(ARConstants.laneDefaultColor.x),
+    var color = UIColor(red: CGFloat(ARConstants.laneDefaultColor.x),
                         green: CGFloat(ARConstants.laneDefaultColor.y),
                         blue: CGFloat(ARConstants.laneDefaultColor.z),
                         alpha: CGFloat(ARConstants.laneDefaultColor.w))
-        width = 1.0
-        lightPosition = WorldCoordinate(x: 0, y: 0, z: 0)
-        lightColor = UIColor.white
-        ambientColor = UIColor(red: CGFloat(ARConstants.laneDefaultColor.x),
+    /// Width of the lane.
+    var width: Float = 1.0
+    /// Position of the light source.
+    var lightPosition = WorldCoordinate(x: 0, y: 0, z: 0)
+    /// Color of the light source. It does not take alpha channel into account.
+    var lightColor = UIColor.white
+    /// Ambient light color represented in RGB. It does not take alpha channel into account.
+    var ambientColor = UIColor(red: CGFloat(ARConstants.laneDefaultColor.x),
                                green: CGFloat(ARConstants.laneDefaultColor.y),
                                blue: CGFloat(ARConstants.laneDefaultColor.z),
                                alpha: 1.0)
-    }
 }
