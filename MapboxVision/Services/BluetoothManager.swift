763eb89005cc0df7917eae0e7ad060baf371a2b0
Dersim Davaod <dersim.davaod@mapbox.com>
2019-07-17 21:21:09 +0300

    Add BluetoothManager implementation based on CoreBluetooth.

--- MapboxVision/Services/BluetoothManager.swift
+++ MapboxVision/Services/BluetoothManager.swift
@@ -1,1 +1,24 @@
+import CoreBluetooth
 
+final class BluetoothManager: NSObject {
+    private(set) var isBluetoothPoweredOn = false
+
+    private var coreBluetoothManager: CBCentralManager?
+    private let bluetoothManagerQueue = DispatchQueue(label: "com.mapbox.BluetoothManager")
+
+    override init() {
+        super.init()
+        coreBluetoothManager = CBCentralManager(delegate: self, queue: bluetoothManagerQueue)
+    }
+}
+
+extension BluetoothManager: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        switch central.state {
+        case .poweredOn:
+            isBluetoothPoweredOn = true
+        default:
+            isBluetoothPoweredOn = false
+        }
+    }
+}
