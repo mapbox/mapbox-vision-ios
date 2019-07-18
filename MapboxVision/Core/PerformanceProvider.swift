6ea17269f8c92ccc207aa4eccbf55ea3fd27905f
Dersim Davaod <dersim.davaod@mapbox.com>
2019-07-18 22:52:14 +0300

    Add PerformanceProvider implementation.

--- MapboxVision/Core/PerformanceProvider.swift
+++ MapboxVision/Core/PerformanceProvider.swift
@@ -1,1 +1,13 @@
+final class PerformanceProvider: NSObject {
+    struct Dependencies {
+        let reachability: Reachability
+        let bluetoothManager: BluetoothManager
+        let batteryManager: BatteryManager
+    }
 
+    private let dependencies: Dependencies
+
+    init(dependencies: Dependencies) {
+        self.dependencies = dependencies
+    }
+}
