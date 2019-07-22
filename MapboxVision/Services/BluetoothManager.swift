4a7095b235b59909d5408d23ce6639489e0e4341
Dersim Davaod <dersim.davaod@mapbox.com>
2019-07-22 21:28:26 +0300

    Add initial doc comments to BluetoothManager.swift.

--- MapboxVision/Services/BluetoothManager.swift
+++ MapboxVision/Services/BluetoothManager.swift
@@ -1,11 +1,25 @@
 import CoreBluetooth
 
+/// Class that contains logic to dispatch and manage the bluetooth module.
 final class BluetoothManager: NSObject {
+
+    // MARK: Public properties
+
+    /// Indicates whether the bluetooth module is powered on.
     private(set) var isBluetoothPoweredOn = false
 
+    // MARK: Private properties
+
+    /// Underlying `CBCentralManager` object that manages state of bluetooth module.
     private var coreBluetoothManager: CBCentralManager?
+    /// The dispatch queue to dispatch the CBCentralManager's events.
     private let bluetoothManagerQueue = DispatchQueue(label: "com.mapbox.BluetoothManager")
 
+    // MARK: Lifecycle
+
+    /**
+     Base initializer to create an instance of `BluetoothManager`.
+     */
     override init() {
         super.init()
         coreBluetoothManager = CBCentralManager(delegate: self, queue: bluetoothManagerQueue)
