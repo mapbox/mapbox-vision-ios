e7b021490de23a7480614746d6b9cc4bb5019b07
Dersim Davaod <dersim.davaod@mapbox.com>
2019-07-27 17:47:32 +0300

    Update type that is used to define memory capacity from `UInt64` to `Byte`.

--- MapboxVision/Services/Recording/FileManager.swift
+++ MapboxVision/Services/Recording/FileManager.swift
@@ -5,11 +5,11 @@ protocol FileManagerProtocol {
     func contentsOfDirectory(at url: URL) throws -> [URL]
     func fileExists(atPath path: String) -> Bool
     func createFile(atPath path: String, contents: Data?) -> Bool
-    func fileSize(at url: URL) -> UInt64
+    func fileSize(at url: URL) -> Byte
 }
 
 extension FileManagerProtocol {
-    func sizeOfDirectory(at url: URL) -> UInt64 {
+    func sizeOfDirectory(at url: URL) -> Byte {
         guard let contents = try? self.contentsOfDirectory(at: url) else {
             return 0
         }
@@ -27,8 +27,8 @@ extension FileManager: FileManagerProtocol {
         return try contentsOfDirectory(at: url, includingPropertiesForKeys: nil, options: [])
     }
 
-    func fileSize(at url: URL) -> UInt64 {
+    func fileSize(at url: URL) -> Byte {
         guard let attributes = try? attributesOfItem(atPath: url.path) else { return 0 }
-        return attributes[FileAttributeKey.size] as? UInt64 ?? 0
+        return attributes[FileAttributeKey.size] as? Byte ?? 0
     }
 }
--- MapboxVisionTests/Mocks/MockFileManager.swift
+++ MapboxVisionTests/Mocks/MockFileManager.swift
@@ -4,7 +4,7 @@ import Foundation
 final class MockFileManager: FileManagerProtocol {
     struct File {
         let url: URL
-        let size: UInt64
+        let size: Byte
     }
 
     var data: [File] = []
@@ -29,7 +29,7 @@ final class MockFileManager: FileManagerProtocol {
         return true
     }
 
-    func fileSize(at url: URL) -> UInt64 {
+    func fileSize(at url: URL) -> Byte {
         return data.first { $0.url == url }?.size ?? 0
     }
 }
