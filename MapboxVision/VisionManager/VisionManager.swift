c45df6ef8ca6a9074dbc2242cfbb7b7c070e2bfe
Alexander Pristavko <pristavkoal@gmail.com>
2019-08-12 15:19:25 +0300

    Move region to country conversion out of VisionManager

--- MapboxVision/VisionManager/VisionManager.swift
+++ MapboxVision/VisionManager/VisionManager.swift
@@ -284,16 +284,3 @@ extension VisionManager: RecordCoordinatorDelegate {
         try recordingPath.move(to: .recordings(syncRegion))
     }
 }
-
-private extension Country {
-    var syncRegion: SyncRegion? {
-        switch self {
-        case .USA, .UK, .other:
-            return .other
-        case .china:
-            return .china
-        case .unknown:
-            return nil
-        }
-    }
-}
