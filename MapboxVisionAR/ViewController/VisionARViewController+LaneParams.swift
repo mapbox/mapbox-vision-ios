d5a41e46c93a05ca9794252b40c60e9fdd877cc3
Mikhail Ioda <mikhailiodo@gmail.com>
2019-08-30 13:09:06 +0300

    Review fixes

---
+++ MapboxVisionAR/ViewController/VisionARViewController+LaneParams.swift
@@ -0,0 +1,8 @@
+import MapboxVisionARNative
+import UIKit
+
+extension VisionARViewController {
+    public func set(laneVisualParams: LaneVisualParams) {
+        self.laneVisualParams = laneVisualParams
+    }
+}
