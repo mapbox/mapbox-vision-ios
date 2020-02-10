0f139f23a6630549f6b421669abae1a09b6fa5e1
Alexander Pristavko <pristavkoal@gmail.com>
2020-02-10 21:03:41 +0300

    Add tests for performance config to performance translation

--- MapboxVisionTests/Mocks/MockNative.swift
+++ MapboxVisionTests/Mocks/MockNative.swift
@@ -2,21 +2,11 @@
 @testable import MapboxVisionNative
 
 class MockNative: VisionManagerNativeProtocol {
-    var useMergedModel = true
-
     weak var delegate: VisionDelegate?
 
     var videoSource: MBVVideoSource?
 
     private(set) var isDestroyed: Bool = false
-
-    func setSegmentationFixedFPS(_: Float) {}
-
-    func setSegmentationDynamicFPS(minFPS: Float, maxFPS: Float) {}
-
-    func setDetectionFixedFPS(_: Float) {}
-
-    func setDetectionDynamicFPS(minFPS: Float, maxFPS: Float) {}
 
     var config = CoreConfig()
 
@@ -36,14 +26,6 @@ class MockNative: VisionManagerNativeProtocol {
         isDestroyed = true
     }
 
-    func getSeconds() -> Float {
-        return 0
-    }
-
-    func startSavingSession(_ path: String) {}
-
-    func stopSavingSession() {}
-
     func pixel(toWorld screenCoordinate: Point2D) -> WorldCoordinate? {
         return nil
     }
@@ -60,7 +42,7 @@ class MockNative: VisionManagerNativeProtocol {
         return nil
     }
 
-    func setFixedFPS(for modelType: MLModelType, FPS: Float) {}
+    func setFixedFPS(_ fps: Float) {}
 
-    func setDynamicFPS(for modelType: MLModelType, minFPS: Float, maxFPS: Float) {}
+    func setDynamicFPS(minFPS: Float, maxFPS: Float) {}
 }
