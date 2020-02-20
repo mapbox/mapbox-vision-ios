2cdefde36df1ff2435f228942a73c325250a317e
Alexander Pristavko <pristavkoal@gmail.com>
2020-02-20 22:26:37 +0300

    Add logging for Events manager

--- MapboxVision/Services/EventsManager.swift
+++ MapboxVision/Services/EventsManager.swift
@@ -4,6 +4,7 @@ import MapboxVisionNative
 
 final class EventsManager {
     private let manager = MMEEventsManager()
+    private let recorder: FileRecorder
 
     private lazy var accessToken: String = {
         guard
@@ -21,11 +22,23 @@ final class EventsManager {
         let name = bundle.infoDictionary!["CFBundleName"] as! String
         let version = bundle.infoDictionary!["CFBundleShortVersionString"] as! String
 
+        let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+        let formatter = DateFormatter()
+        formatter.dateFormat = "y-MM-dd_HH-mm-ss"
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        let path = documentsPath.appending("/vision_log-\(formatter.string(from: Date())).json")
+
+        recorder = FileRecorder(path: path)!
+
+        recorder.record(Log("Initialize: name: \(name), version: \(version)"))
         manager.initialize(
             withAccessToken: accessToken,
             userAgentBase: name,
             hostSDKVersion: version
         )
+        manager.isDebugLoggingEnabled = true
+
+        recorder.record(Log("Send turnstile event"))
         manager.sendTurnstileEvent()
         manager.isMetricsEnabled = true
     }
@@ -33,17 +46,80 @@ final class EventsManager {
 
 extension EventsManager: NetworkClient {
     func set(baseURL: URL?) {
+        recorder.record(Log("Set base url: \(baseURL.debugDescription)"))
         manager.baseURL = baseURL
+        recorder.record(Log("Send turnstile event after set base url: \(baseURL.debugDescription)"))
         manager.sendTurnstileEvent()
     }
 
     func sendEvent(name: String, entries: [String: Any]) {
+        recorder.record(Log("Send event: name: \(name), entries: \(entries)"))
         manager.enqueueEvent(withName: name, attributes: entries)
     }
 
     func upload(file: String, metadata: TelemetryFileMetadata, completion: @escaping (Error?) -> Void) {
+        recorder.record(Log("Post metadata: file: \(file), metadata: \(metadata)"))
         manager.postMetadata([metadata], filePaths: [file], completionHandler: completion)
     }
 
     func cancel() {}
 }
+
+struct Log: Codable {
+    let date: String
+    let message: String
+
+    init(_ message: String) {
+        self.date = iso8601DateFormatter.string(from: Date())
+        self.message = message
+    }
+}
+
+final class FileRecorder {
+    private let stream: OutputStream
+    private var firstChunk = true
+
+    init?(path: String) {
+        guard let stream = OutputStream(toFileAtPath: path, append: true) else { return nil }
+        self.stream = stream
+        stream.open()
+    }
+
+    func record<T: Encodable>(_ info: T) {
+        if firstChunk {
+            firstChunk = false
+        } else {
+            stream.write(string: ",")
+        }
+
+        guard let encoded = try? JSONEncoder().encode(info)
+        else { assertionFailure("Can't encode metainfo record to json data"); return }
+
+        guard let jsonObject = try? JSONSerialization.jsonObject(with: encoded, options: [])
+        else { assertionFailure("Can't convert encoded data to json object"); return }
+
+        var error: NSError?
+        JSONSerialization.writeJSONObject(jsonObject, to: self.stream, options: JSONSerialization.WritingOptions.prettyPrinted, error: &error)
+        if let error = error { assertionFailure(error.localizedDescription); return }
+    }
+
+    deinit {
+        stream.close()
+    }
+}
+
+private extension OutputStream {
+    func write(string: String) {
+        _ = string.data(using: .utf8)?.withUnsafeBytes { ptr in
+            self.write(ptr, maxLength: string.lengthOfBytes(using: .utf8))
+        }
+    }
+}
+
+let iso8601DateFormatter: DateFormatter = {
+    let dateFormatter = DateFormatter()
+    dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+    dateFormatter.timeZone = TimeZone(identifier: "UTC")
+    return dateFormatter
+}()
