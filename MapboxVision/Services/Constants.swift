b9cd24902dabfe4d4eb1ecd2391e2b2b4a8bb06a
Mikhail Ioda <mikhailiodo@gmail.com>
2020-03-17 12:41:03 +0300

    Fix wrong type

--- MapboxVision/Services/Constants.swift
+++ MapboxVision/Services/Constants.swift
@@ -4,7 +4,7 @@ enum Constants {
     static let motionUpdateInterval: TimeInterval = 1 / 30.0
     static let millisecondsInSecond: Double = 1000
     static let frameDuration: TimeInterval = 1 / 30.0
-    static let preferredTimescale: Int = 600
+    static let preferredTimescale: Int32 = 600
 
     enum URL {
         static let defaultEventsEndpoint = "https://events.mapbox.com"
