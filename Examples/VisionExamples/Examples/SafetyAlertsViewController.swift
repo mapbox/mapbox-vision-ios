347052e8599145a51858c6a1608b549b83b6668f
Dersim Davaod <dersim.davaod@mapbox.com>
2020-04-07 01:54:34 +0300

    Update Safety Alerts example.

--- Examples/VisionExamples/Examples/SafetyAlertsViewController.swift
+++ Examples/VisionExamples/Examples/SafetyAlertsViewController.swift
@@ -7,13 +7,28 @@ import UIKit
  * to alert a user about exceeding allowed speed limit and potential collisions with other cars.
  */
 
+// Custom UIView to draw a red bounding boxes
+class CollisionDetectionView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        // Transparent view with a red border
+        backgroundColor = .clear
+        layer.borderWidth = 3
+        layer.borderColor = UIColor.red.cgColor
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
 class SafetyAlertsViewController: UIViewController {
     private var visionManager: VisionReplayManager!
     private var visionSafetyManager: VisionSafetyManager!
 
     private let visionViewController = VisionPresentationViewController()
-    
-    private var alertNoOverspeedingView: UIView!
+
     private var alertOverspeedingView: UIView!
 
     private var vehicleState: VehicleState?
@@ -22,19 +37,19 @@ class SafetyAlertsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Documents directory path with files uploaded via Finder
         let documentsPath =
             NSSearchPathForDirectoriesInDomains(.documentDirectory,
                                                 .userDomainMask,
                                                 true).first!
         let path = documentsPath.appending("/safety-alerts-drawing")
-        
+
         // create VisionReplayManager with a path to recorded session
         visionManager = try? VisionReplayManager.create(recordPath: path)
         // register its delegate
         visionManager.delegate = self
-        
+
         // create VisionSafetyManager and register as its delegate to receive safety related events
         visionSafetyManager = VisionSafetyManager.create(visionManager: visionManager)
         // register its delegate
@@ -45,8 +60,8 @@ class SafetyAlertsViewController: UIViewController {
         // add Vision view as a child view
         addVisionView()
 
-        // add views to draw overspeeding alert
-        addOverspeedingAlertViews()
+        // add view to draw overspeeding alert
+        addOverspeedingAlertView()
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -60,7 +75,7 @@ class SafetyAlertsViewController: UIViewController {
 
         visionManager.stop()
     }
-    
+
     deinit {
         // free up VisionManager's resources
         visionManager.destroy()
@@ -72,20 +87,9 @@ class SafetyAlertsViewController: UIViewController {
         visionViewController.didMove(toParent: self)
     }
 
-    private func addOverspeedingAlertViews() {
-        alertNoOverspeedingView = UIImageView(image: UIImage(named: "alert"))
-        alertNoOverspeedingView.isHidden = true
-        alertNoOverspeedingView.tintColor = UIColor.green
-        alertNoOverspeedingView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(alertNoOverspeedingView)
-        NSLayoutConstraint.activate([
-            alertNoOverspeedingView.topAnchor.constraint(equalToSystemSpacingBelow: view.topAnchor, multiplier: 1),
-            view.trailingAnchor.constraint(equalToSystemSpacingAfter: alertNoOverspeedingView.trailingAnchor, multiplier: 1)
-        ])
-        
+    private func addOverspeedingAlertView() {
         alertOverspeedingView = UIImageView(image: UIImage(named: "alert"))
         alertOverspeedingView.isHidden = true
-        alertOverspeedingView.tintColor = UIColor.red
         alertOverspeedingView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(alertOverspeedingView)
         NSLayoutConstraint.activate([
@@ -94,16 +98,37 @@ class SafetyAlertsViewController: UIViewController {
         ])
     }
 
-    private func updateCollisionDrawing() {
-        clearCollisionStateDrawings()
-        for carCollision in carCollisions {
-            let bbox = carCollision.lastDetection.boundingBox
-            let bboxColor = carCollision.dangerLevel == .none ? UIColor.green : UIColor.red
+    // MARK: - Drawing custom collision detection views
 
-            drawCollisionState(in: bbox, with: bboxColor)
+    private func clearCollisionStateDrawings() {
+        for subview in view.subviews {
+            if subview.isKind(of: CollisionDetectionView.self) {
+                subview.removeFromSuperview()
+            }
         }
     }
-    
+
+    // MARK: - Handle VisionSafety events
+
+    private func updateCollisionDrawing() {
+        clearCollisionStateDrawings()
+
+        for carCollision in carCollisions {
+            // Calculate absolute coordinates
+            let relativeBBox = carCollision.lastDetection.boundingBox
+            let frameSize = carCollision.lastFrame.image.size.cgSize
+
+            let bbox = CGRect(x: relativeBBox.origin.x * frameSize.width,
+                              y: relativeBBox.origin.y * frameSize.height,
+                              width: relativeBBox.size.width * frameSize.width,
+                              height: relativeBBox.size.height * frameSize.height)
+
+            // Draw a collision detection alert
+            let view = CollisionDetectionView(frame: bbox)
+            self.view.addSubview(view)
+        }
+    }
+
     private func updateOverspeedingDrawing() {
         // when update is completed all the data has the most current state
         guard let vehicle = self.vehicleState, let limits = self.speedLimits else { return }
@@ -111,7 +136,6 @@ class SafetyAlertsViewController: UIViewController {
         // decide whether speed limit is exceeded by comparing it with the current speed
         let isOverSpeeding = vehicle.speed > limits.speedLimitRange.max
         self.alertOverspeedingView.isHidden = !isOverSpeeding
-        self.alertNoOverspeedingView.isHidden = isOverSpeeding
     }
 }
 
@@ -124,7 +148,7 @@ extension SafetyAlertsViewController: VisionManagerDelegate {
             self?.vehicleState = vehicleState
         }
     }
-    
+
     func visionManagerDidCompleteUpdate(_ visionManager: VisionManagerProtocol) {
         // dispatch to the main queue in order to work with UIKit elements
         DispatchQueue.main.async { [weak self] in
@@ -144,13 +168,11 @@ extension SafetyAlertsViewController: VisionSafetyManagerDelegate {
             self?.speedLimits = roadRestrictions.speedLimits
         }
     }
-    
+
     func visionSafetyManager(_ visionSafetyManager: VisionSafetyManager,
                              didUpdateCollisions collisions: [CollisionObject]) {
-        let carCollisions = collisions.compactMap { collision -> CollisionObject? in
-            return collision.object.detectionClass == .car ? collision : nil
-        }
-        
+        let carCollisions = collisions.filter { $0.object.detectionClass == .car }
+
         // dispatch to the main queue in order to sync access to `[CollisionObject]` array
         DispatchQueue.main.async { [weak self] in
             // update current collisions state
