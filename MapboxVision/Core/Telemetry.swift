4cb5522a0fd47f13b9bd8386f138a2ed7e365185
Alexander Pristavko <pristavkoal@gmail.com>
2020-06-23 20:49:59 +0300

    Return the proper init

--- MapboxVision/Core/Telemetry.swift
+++ MapboxVision/Core/Telemetry.swift
@@ -4,6 +4,32 @@ import MapboxMobileEvents
 final class Telemetry: NSObject, TelemetryInterface {
     private let manager = MMEEventsManager()
 
+    private lazy var accessToken: String = {
+        guard
+            let dict = Bundle.main.infoDictionary,
+            let token = dict["MGLMapboxAccessToken"] as? String
+        else {
+            assertionFailure("accessToken must be set in the Info.plist as MGLMapboxAccessToken.")
+            return ""
+        }
+        return token
+    }()
+
+    override init() {
+        super.init()
+
+        let bundle = Bundle(for: type(of: self))
+        let name = bundle.infoDictionary!["CFBundleName"] as! String
+        let version = bundle.infoDictionary!["CFBundleShortVersionString"] as! String
+
+        manager.initialize(
+            withAccessToken: accessToken,
+            userAgentBase: name,
+            hostSDKVersion: version
+        )
+        manager.sendTurnstileEvent()
+    }
+
     func setSyncUrl(_: String, isChina: Bool) {
         UserDefaults.mme_configuration().mme_isCNRegion = isChina
         manager.sendTurnstileEvent()
