0eeda27a96c5077af7665bcd319474e1e98094ec
Dersim Davaod <dersim.davaod@mapbox.com>
2021-06-09 16:22:58 +0300

    Allow portrait mode temporarily on a platform.

--- MapboxVision/Helpers/AVCaptureConnection+Orientation.swift
+++ MapboxVision/Helpers/AVCaptureConnection+Orientation.swift
@@ -5,7 +5,6 @@ import UIKit
 extension AVCaptureConnection {
     func set(deviceOrientation: UIDeviceOrientation) {
         if isVideoOrientationSupported,
-            deviceOrientation.isLandscape,
             let videoOrientation = AVCaptureVideoOrientation(rawValue: deviceOrientation.rawValue),
             self.videoOrientation != videoOrientation {
             self.videoOrientation = videoOrientation
